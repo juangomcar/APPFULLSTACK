f166af82a1327f03dfa070f15e7626ac69d00a5f
DanielPPPf <danielpf.spotify@gmail.com>
2024-09-18 22:06:41 -0500

    Fix next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, 
+    pageExtensions: ['tsx', 'ts', 'jsx', 'js'], 
+  };
+  
+  export default nextConfig;
+  
